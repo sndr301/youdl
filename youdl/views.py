9a94535a0eb9486af30d1255a6b144918dbc440e
Toby Bonhof <toby@the-saints.nl>
2015-01-26 12:18:20 +0100

    Changed available param to be full url

--- youdl/views.py
+++ youdl/views.py
@@ -11,9 +11,9 @@ class GetUrl(views.APIView):
     	content = {}
     	youtube = YoutubeDL()
         you_id = request.data['ID']
-        if len(you_id) != 11:
-        	return Response(status=404)
-        you_url = "http://youtube.com/watch?v=" + you_id
+        # if len(you_id) != 11:
+        # 	return Response(status=404)
+        # you_url = "http://youtube.com/watch?v=" + you_id
         try:
             info = youtube.extract_info(you_url, download=False)
         except:
