52efd71a66249e1aaf1b9e628d19ed095352c509
Toby Bonhof <toby@the-saints.nl>
2015-01-26 12:26:49 +0100

    Changed ID to contain a url of a third party service like Vimeo

--- youdl/views.py
+++ youdl/views.py
@@ -11,11 +11,8 @@ class GetUrl(views.APIView):
     	content = {}
     	youtube = YoutubeDL()
         you_id = request.data['ID']
-        # if len(you_id) != 11:
-        # 	return Response(status=404)
-        # you_url = "http://youtube.com/watch?v=" + you_id
         try:
-            info = youtube.extract_info(you_url, download=False)
+            info = youtube.extract_info(you_id, download=False)
         except:
             return Response(status=404)
         content['url'] = info['url']
